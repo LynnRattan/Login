7c5373ce6ed196e9f0689a6fbcefb2ab8f011165
User <User@LAB3-3>
2024-02-05 13:10:46 +0200

    Add project files.

---
+++ Login/Services/LoginService.cs
@@ -0,0 +1,32 @@
+﻿using Login.Models;
+using CoreML;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login.Services
+{
+    class UsersList
+    {
+        public List<User> Users { get; private set; }
+        public UsersList()
+        {
+            this.Users = new List<User>();
+            Users.Add(new User());
+        }
+    }
+        public class LoginService
+    {
+        public LoginService()
+        {
+            this.Users = new UsersList().Users;
+        }
+        List<User> Users;
+        public User FindUserByNameAndPassword(string name,string password)
+        {
+            return Users.Where(x => x.Name == name && x.Password==password).FirstOrDefault();
+        }
+    }
+}
---
+++ Login/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,52 @@
+﻿using CoreML;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Login.Models;
+using Login.Services;
+
+namespace Login.ViewModels
+{
+    public class LoginPageViewModel:ViewModel
+    {
+        private string name;
+        private string password;
+        public string Name { get { return name; } set { name = value; OnPropertyChanged(); ((Command)SearchByNameCommand).ChangeCanExecute(); } }
+
+        public string Password { get { return password; } set { password = value; OnPropertyChanged(); ((Command)SearchByPasswordCommand).ChangeCanExecute(); } }
+
+        public ICommand SearchByNameCommand { get; set; }
+        private User user;
+        public ICommand SearchByPasswordCommand { get; set; }
+
+        
+        public LoginageViewModel()
+        {
+            user = new User() { Name = "התחברות נכשלה" };
+            SearchByNameCommand = new Command(FindUserByName(Name), () => !String.IsNullOrEmpty(Name));
+        }
+
+        private void FindUser()
+        {
+            LoginService service = new LoginService();
+            List<Monkey> lst = service.FindMonkeysByLocation(Country);
+            if (lst.Count > 0)
+                monkey = lst[0];
+            else
+                monkey = new Monkey() { Name = "אין קופים להצגה" };
+            Count = lst.Count;
+            RefreshData();
+            Country = null;
+        }
+
+        public void RefreshData()
+        {
+            OnPropertyChanged("Name");
+            OnPropertyChanged(nameof(ImageUrl));
+        }
+    }
+}
